7181864e8ebe43001364cc74dd0d226bc9020ae4
Alastair McNeill <alastair.r.mcneill@gmail.com>
2025-08-07 22:08:01 +0100

    Story 1.1 Project setup

---
+++ ScreenshotEditor/ViewModels/ViewModelPlaceholder.swift
@@ -0,0 +1,12 @@
+//
+//  ViewModelPlaceholder.swift
+//  ScreenshotEditor
+//
+//  Created on 7/8/2025.
+//
+
+import Foundation
+
+// MARK: - ViewModels Directory Placeholder
+// This file exists to maintain the ViewModels directory structure.
+// Remove this file when adding the first actual view model.
