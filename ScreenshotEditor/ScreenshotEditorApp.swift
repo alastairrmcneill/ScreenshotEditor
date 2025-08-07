1172264fd8c65be24e70cdc998af72374964f16a
Alastair McNeill <alastair.r.mcneill@gmail.com>
2025-08-07 07:58:40 +0100

    Initial Commit

---
+++ ScreenshotEditor/ScreenshotEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenshotEditorApp.swift
+//  ScreenshotEditor
+//
+//  Created by Alastair McNeill on 07/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct ScreenshotEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ScreenshotEditorTests/ScreenshotEditorTests.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenshotEditorTests.swift
+//  ScreenshotEditorTests
+//
+//  Created by Alastair McNeill on 07/08/2025.
+//
+
+import Testing
+@testable import ScreenshotEditor
+
+struct ScreenshotEditorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
